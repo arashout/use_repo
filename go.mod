e8f1b5906993396ea94cb59473706a140f6dcd84
arash outadi <arash.out@gmail.com>
2023-01-31 15:22:52 -0800

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/arashout/use_repo
+
+go 1.19
+
+require github.com/arashout/fake_monorepo/go/src/fakeorg/artifactory/anothertestpackage v0.1.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/arashout/fake_monorepo/go/src/fakeorg/artifactory/anothertestpackage/beef"
+)
+
+func main() {
+	fmt.Println(beef.SirloinSteak())
+}
